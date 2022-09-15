72c3360c5736f6b9e10d30ec707ed5a0b2e3dea7
cino <cuneyt.tai@gmail.com>
2022-09-15 10:19:15 +0200

    update sous cat

--- src/DataFixtures/SousCategorieFixtures.php
+++ src/DataFixtures/SousCategorieFixtures.php
@@ -3,9 +3,10 @@
 namespace App\DataFixtures;
 
 use App\Entity\SousCategorie;
+use App\DataFixtures\CategorieFixtures;
+use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
-use Doctrine\Persistence\ObjectManager;
 
 class SousCategorieFixtures extends Fixture implements DependentFixtureInterface
 {
@@ -27,6 +28,70 @@ class SousCategorieFixtures extends Fixture implements DependentFixtureInterface
         $sousCategorie->setUpdatedAt(new \DateTimeImmutable());
         $manager->persist($sousCategorie);
 
+        $sousCategorie = new SousCategorie();
+        $sousCategorie->setNom('Peinture');
+        $sousCategorie->setSlug('peinture');
+        $sousCategorie->setImageName('peinture.jpeg');
+        $sousCategorie->setCategorie($this->getReference(CategorieFixtures::CAT_BRICOLAGE));
+        $sousCategorie->setUpdatedAt(new \DateTimeImmutable());
+        $manager->persist($sousCategorie);
+
+        $sousCategorie = new SousCategorie();
+        $sousCategorie->setNom('Plomberie');
+        $sousCategorie->setSlug('plomberie');
+        $sousCategorie->setImageName('plomberie.jpeg');
+        $sousCategorie->setCategorie($this->getReference(CategorieFixtures::CAT_BRICOLAGE));
+        $sousCategorie->setUpdatedAt(new \DateTimeImmutable());
+        $manager->persist($sousCategorie);
+
+        $sousCategorie = new SousCategorie();
+        $sousCategorie->setNom('Electricité');
+        $sousCategorie->setSlug('electricite');
+        $sousCategorie->setImageName('electricite.jpeg');
+        $sousCategorie->setCategorie($this->getReference(CategorieFixtures::CAT_BRICOLAGE));
+        $sousCategorie->setUpdatedAt(new \DateTimeImmutable());
+        $manager->persist($sousCategorie);
+
+        $sousCategorie = new SousCategorie();
+        $sousCategorie->setNom('Menuiserie');
+        $sousCategorie->setSlug('menuiserie');
+        $sousCategorie->setImageName('menuiserie.jpeg');
+        $sousCategorie->setCategorie($this->getReference(CategorieFixtures::CAT_BRICOLAGE));
+        $sousCategorie->setUpdatedAt(new \DateTimeImmutable());
+        $manager->persist($sousCategorie);
+
+        $sousCategorie = new SousCategorie();
+        $sousCategorie->setNom('Jardinage');
+        $sousCategorie->setSlug('jardinage');
+        $sousCategorie->setImageName('jardinage.jpeg');
+        $sousCategorie->setCategorie($this->getReference(CategorieFixtures::CAT_BRICOLAGE));
+        $sousCategorie->setUpdatedAt(new \DateTimeImmutable());
+        $manager->persist($sousCategorie);
+
+        $sousCategorie = new SousCategorie();
+        $sousCategorie->setNom('Décoration');
+        $sousCategorie->setSlug('decoration');
+        $sousCategorie->setImageName('decoration.jpeg');
+        $sousCategorie->setCategorie($this->getReference(CategorieFixtures::CAT_BRICOLAGE));
+        $sousCategorie->setUpdatedAt(new \DateTimeImmutable());
+        $manager->persist($sousCategorie);
+
+        $sousCategorie = new SousCategorie();
+        $sousCategorie->setNom('Mobilier');
+        $sousCategorie->setSlug('mobilier');
+        $sousCategorie->setImageName('mobilier.jpeg');
+        $sousCategorie->setCategorie($this->getReference(CategorieFixtures::CAT_BRICOLAGE));
+        $sousCategorie->setUpdatedAt(new \DateTimeImmutable());
+        $manager->persist($sousCategorie);
+
+        $sousCategorie = new SousCategorie();
+        $sousCategorie->setNom('Cuisine');
+        $sousCategorie->setSlug('cuisine');
+        $sousCategorie->setImageName('cuisine.jpeg');
+        $sousCategorie->setCategorie($this->getReference(CategorieFixtures::CAT_BRICOLAGE));
+        $sousCategorie->setUpdatedAt(new \DateTimeImmutable());
+        $manager->persist($sousCategorie);
+        
         $manager->flush();
     }
 
